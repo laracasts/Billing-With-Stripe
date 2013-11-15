7b726a88c05083454a763c0cacff73771ad45f75
Jeffrey Way <jeffrey@envato.com>
2013-11-15 13:41:31 -0500

    Set keys

--- app/config/development/stripe.php
+++ app/config/development/stripe.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'secret_key' => 'sk_test_bwzU1gcoqctzXmLmYJ6vXAkI',
-    'publishable_key' => 'pk_test_vEql1qFyFbL8Us2ok1wz8liH'
+    'secret_key' => 'TEST SECRET KEY',
+    'publishable_key' => 'TEST PUBLISHABLE KEY'
 ];
--- app/config/stripe.php
+++ app/config/stripe.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'secret_key' => 'sk_live_f8T9iDAQT1k68MPL1CjcsXv9',
-    'publishable_key' => 'pk_live_PiqylWO3L0MLRGUqFK9i65vg'
+    'secret_key' => 'PRODUCTION SECRET KEY',
+    'publishable_key' => 'PRODUCTION PUBLISHABLE KEY'
 ];
